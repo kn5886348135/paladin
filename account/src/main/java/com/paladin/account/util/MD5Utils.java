88ae4f8012948f6f1bf97c823b879c41309a4e3c
kn5886348135 <kn5886348135@163.com>
2020-10-08 22:19:07 +0800

    添加MD5签名工具类

---
+++ account/src/main/java/com/paladin/account/util/MD5Utils.java
@@ -0,0 +1,34 @@
+package com.paladin.account.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @Description: MD5签名
+ * @Author: paladin
+ * @date: 2020/10/8 22:14
+ */
+public class MD5Utils {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MD5Utils.class);
+
+    public static String stringToMD5(String plainText) {
+        byte[] secretBytes = null;
+        try {
+            secretBytes = MessageDigest.getInstance("md5").digest(
+                    plainText.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("没有这个md5算法！");
+        }
+        String md5code = new BigInteger(1, secretBytes).toString(16);
+        for (int i = 0; i < 32 - md5code.length(); i++) {
+            md5code = "0" + md5code;
+        }
+        return md5code;
+    }
+
+}
