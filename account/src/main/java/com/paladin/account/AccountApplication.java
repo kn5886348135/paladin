b6d085b31e05e7606c0f5f1792bf5d339a471abc
kn5886348135 <kn5886348135@163.com>
2020-10-10 20:57:08 +0800

    删除客户端服务注册，单机启动调试

--- account/src/main/java/com/paladin/account/AccountApplication.java
+++ account/src/main/java/com/paladin/account/AccountApplication.java
@@ -3,10 +3,8 @@ package com.paladin.account;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @MapperScan("com.paladin.mapper")
 public class AccountApplication {
 
