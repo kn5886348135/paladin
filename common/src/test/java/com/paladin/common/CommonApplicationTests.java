997c2bec837f86aaf6c8b6f0b0b0e57d66c0d411
kn5886348135 <kn5886348135@163.com>
2023-10-05 00:00:10 +0800

    新增模块

---
+++ common/src/test/java/com/paladin/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paladin.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order/src/main/java/com/paladin/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.paladin.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
